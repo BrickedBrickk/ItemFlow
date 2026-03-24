e194381c526d9ce919de908acb7e025454be5831
DeltaPv <eliboryczewski@icloud.com>
2026-03-23 21:03:50 -0400

    Release v1.0.2

--- src/main/java/com/itemflow/mixin/BarrelBlockEntityMixin.java
+++ src/main/java/com/itemflow/mixin/BarrelBlockEntityMixin.java
@@ -2,6 +2,7 @@ package com.itemflow.mixin;
 
 import com.itemflow.Utils.ChestClosedCallback;
 import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.entity.ContainerUser;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BarrelBlockEntityMixin
 {
     @Inject(at = @At("TAIL"), method = "onClose")
-    public void onClose(PlayerEntity player, CallbackInfo ci)
+    public void onClose(ContainerUser containerUser, CallbackInfo ci)
     {
-        ChestClosedCallback.EVENT.invoker().invoke(player);
+        if (containerUser instanceof PlayerEntity player)
+        {
+            ChestClosedCallback.EVENT.invoker().invoke(player);
+        }
     }
 }
